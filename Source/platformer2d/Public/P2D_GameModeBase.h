f9370361df1ec72b5abe5a9ae5e145d183fd62f1
FilaCo <awesome.chekushka@gmail.com>
2022-05-03 19:57:32 +0500

    Init empty proj

---
+++ Source/platformer2d/Public/P2D_GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "P2D_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMER2D_API AP2D_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/platformer2d/Public/P2D_PaperCharacterBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "P2D_PaperCharacterBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLATFORMER2D_API AP2D_PaperCharacterBase : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
