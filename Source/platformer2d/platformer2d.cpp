88a3b82b45b2c26e9d425516974387ea0652c709
FilaCo <awesome.chekushka@gmail.com>
2022-05-04 16:46:08 +0500

    Init unity

--- Source/platformer2d/platformer2d.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "platformer2d.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, platformer2d, "platformer2d" );
